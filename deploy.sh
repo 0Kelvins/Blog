ff2ece2a2eb2349a626fe38716f9e775f956d247
0Kelvins <554961401@qq.com>
2017-11-06 22:53:47 +0800

    2017-11-06 22:53:47

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-﻿cd ../0Kelvins.github.io
+﻿echo ******************提交Blog******************
+echo ******************添加并提交本地仓库********
+git add .
+git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
+echo ******************推送GitHub****************
+git push origin master
+echo ******************完成**********************
+
+echo ******************提交0Kelvins.github.io****
+cd ../0Kelvins.github.io
 echo ******************更新本地仓库**************
 git pull origin master
 echo ******************更新完成******************
@@ -11,7 +20,7 @@ echo ******************复制完成******************
 cd ../0Kelvins.github.io
 echo ******************添加并提交本地仓库********
 git add .
-git commit -m $(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 echo ******************推送GitHub****************
 git push origin master
 echo ******************完成**********************
