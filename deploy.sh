34a9f37d2f5446833421ad3a96578aa22d7be0e9
0Kelvins <554961401@qq.com>
2017-11-07 21:02:18 +0800

    2017-11-7 21:02:17

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 ﻿#! /bin/bash 
 echo ******************提交Blog******************
+echo ******************更新本地仓库**************
+git pull origin master
+echo ******************更新完成******************
 echo ******************添加并提交本地仓库********
 git add .
 git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
@@ -13,9 +16,8 @@ echo ******************更新本地仓库**************
 git pull origin master
 echo ******************更新完成******************
 
-cd ../Blog
 echo ******************开始复制******************
-cp -avx public/* ../0Kelvins.github.io
+cp -avx ../Blog/public/* .
 echo ******************复制完成******************
 
 cd ../0Kelvins.github.io
@@ -26,4 +28,5 @@ echo ******************推送GitHub****************
 git push origin master
 echo ******************完成**********************
 
+cd ../Blog
 read -p "Press any key to continue."
