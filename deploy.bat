7debf1faa9a3ec53dfd942e4a573594e12a3d2d6
0Kelvins <554961401@qq.com>
2017-11-11 21:41:01 +0800

    2017/11/11 周六 21:41:01.29

--- deploy.bat
+++ deploy.bat
@@ -1,17 +1,21 @@
 @echo off
 
-echo ******************提交Blog******************
+echo ------------------提交Blog------------------
+
 echo ******************更新本地仓库**************
 git pull origin master
 echo ******************更新完成******************
+
 echo ******************添加并提交本地仓库********
 git add .
 git commit -m "%date% %time%"
+
 echo ******************推送GitHub****************
 git push origin master
-echo ******************完成**********************
+echo ******************推送完成******************
 
-echo ******************提交0Kelvins.github.io****
+echo ------------------提交0Kelvins.github.io---
+
 cd ../0Kelvins.github.io
 echo ******************更新本地仓库**************
 git pull origin master
@@ -25,9 +29,11 @@ cd ../0Kelvins.github.io
 echo ******************添加并提交本地仓库********
 git add .
 git commit -m "%date% %time%"
+
 echo ******************推送GitHub****************
 git push origin master
 echo ******************完成**********************
 
 cd ../Blog
+
 Pause
