368a78bcc80a6b4e2bbefcfa7ee4e5e5f67d1ead
0Kelvins <554961401@qq.com>
2017-11-11 21:58:34 +0800

    2017/11/11 周六 21:58:34.26

--- deploy.bat
+++ deploy.bat
@@ -3,8 +3,11 @@
 echo ------------------提交Blog------------------
 
 echo ******************更新本地仓库**************
+set /p updateOp1 = "是否更新（y/n）："
+if "%updateOp%" == "y" (
 git pull origin master
 echo ******************更新完成******************
+)
 
 echo ******************添加并提交本地仓库********
 git add .
@@ -18,8 +21,11 @@ echo ------------------提交0Kelvins.github.io---
 
 cd ../0Kelvins.github.io
 echo ******************更新本地仓库**************
+set /p updateOp2 = "是否更新（y/n）："
+if "%updateOp%" == "y" (
 git pull origin master
 echo ******************更新完成******************
+)
 
 echo ******************开始复制******************
 xcopy /s/e ..\Blog\public . /y
