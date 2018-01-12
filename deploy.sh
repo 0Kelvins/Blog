498e27f8607cb199511ced72299bcee6c1ed4d3c
0Kelvins <554961401@qq.com>
2018-01-12 11:10:19 +0800

    2018-01-12 11:10:18

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo ------------------提交Blog------------------
 
 read -p "是否更新Blog(y/n) :" updateOp1
-if ["$updateOp1"x = "y"x]; then
+if [ "$updateOp1"x = "y"x ]; then
 	echo ******************更新本地仓库**************
 	git pull origin master
 	echo ******************更新完成******************
@@ -22,7 +22,7 @@ echo ------------------提交0Kelvins.github.io----
 cd ../0Kelvins.github.io
 
 read -p "是否更新github pages(y/n) :" updateOp1
-if ["$updateOp1"x = "y"x]; then
+if [ "$updateOp1"x = "y"x ]; then
 	echo ******************更新本地仓库**************
 	git pull origin master
 	echo ******************更新完成******************
